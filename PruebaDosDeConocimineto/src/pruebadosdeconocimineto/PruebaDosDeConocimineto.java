9d85eec7f23d59ac622b157164249231382846ce
Enrique Galeano <enrique.galeano99@unitec.edu>
2017-07-25 08:38:40 -0600

    Agregue una linea de sout

---
+++ PruebaDosDeConocimineto/src/pruebadosdeconocimineto/PruebaDosDeConocimineto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebadosdeconocimineto;
+
+/**
+ *
+ * @author enriquejosegaleanotalavera
+ */
+public class PruebaDosDeConocimineto {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo !!! ");
+		
+	}
+	
+}
